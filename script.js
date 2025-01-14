ffddee78ef7f152f9f00efa6341fa9eab2b04896
Chan Pham <iamchanpham@gmail.com>
2025-01-14 17:21:26 +0000

    coding exercises template

---
+++ script.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')()
+// Task 1: Log a greeting message, "Script running" to the console.
+
+
+// Task 2: Prompt the user to enter their username and save it in a variable called 'userName'. Then, console log "Hello, [userName]!"
+
+
+// Task 3: Declare a variable named 'age' and assign it your age. Then, log a message to the console saying "I'm [age] years old."
+
+
+// Task 4: Create an object named 'class' with properties such as 'className', 'teacher', and 'grade'. Assign appropriate values to these properties.
+
+
+// Task 5: Console log "I'm taking [className] with [teacher]"
+
+
+// Task 6: Write an if/else statement that checks if the grade is equal to or over 63. If it is, print "I am passing", else print "Oh no, I'm not passing YET."
+
